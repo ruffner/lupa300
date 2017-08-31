ba4635849dccba5da477a9d0f55a76b5fabdf870
DESKTOP-01T3UKC\matth <mpru223@g.uky.edu>
2017-08-31 17:36:16 -0400

    added pin defs

--- lupa_firmware/lupa_firmware.ino
+++ lupa_firmware/lupa_firmware.ino
@@ -6,7 +6,24 @@
  */
 
 
-#define LUPA_SPI_CLK    13
+#define LUPA_SPI_CLK      13
+#define LUPA_SPI_DATA     11
+#define LUPA_SPI_EN       9
+#define LUPA_FRAME_VALID  8
+#define LUPA_LINE_VALID   7
+#define LUPA_INT1         26
+#define LUPA_INT2         25
+#define LUPA_INT3         24
+#define LUPA_D9           23
+#define LUPA_D8           22
+#define LUPA_D7           21
+#define LUPA_D6           20
+#define LUPA_D5           19
+#define LUPA_D4           18
+#define LUPA_D3           17
+#define LUPA_D2           16
+#define LUPA_D1           15
+#define LUPA_D0           14
 
 
 void setup() {
