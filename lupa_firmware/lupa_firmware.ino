45a5767559bf81559e92cf413a5495f00edb1674
DESKTOP-01T3UKC\matth <mpru223@g.uky.edu>
2017-08-31 15:48:46 -0400

    setup inital firmware

---
+++ lupa_firmware/lupa_firmware.ino
@@ -0,0 +1,20 @@
+/* Matt Ruffner 
+ * August 2017
+ * 
+ * Firmware for interfacing the LUPA300 
+ * image sensor with an Teensy 3.6
+ */
+
+
+#define LUPA_SPI_CLK    13
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
