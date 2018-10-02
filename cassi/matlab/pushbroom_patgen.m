b0f51a34e41decb0a0fa35773b187467011a261b
Matt Ruffner <mattruffner7@gmail.com>
2018-10-02 15:26:27 -0400

    centered pushbroom patterns

--- cassi/matlab/pushbroom_patgen.m
+++ cassi/matlab/pushbroom_patgen.m
@@ -6,7 +6,7 @@ rows = 1140;
 cols = 912;
 for n=1:32
     x = ones(rows, cols, 3, 'uint8')*255;
-    for m=n:32:cols
+    for m=n+8:32:cols-8
         x(:,m,:)=repmat(rem(1:rows,2)'*255,1,3);
     end
     if (n<10)
